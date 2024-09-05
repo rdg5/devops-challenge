d3e5f3ddcc23b41a76d0c9630b8c4c9276dd3a1f
sanyi <sandor.vass@tuta.io>
2024-09-05 12:47:21 +0200

    feat(devops-challenge): add dockerfile for the apis & change api2 fetch url

--- bird/main.go
+++ bird/main.go
@@ -25,7 +25,7 @@ func defaultBird(err error) Bird {
 }
 
 func getBirdImage(birdName string) (string, error) {
-    res, err := http.Get(fmt.Sprintf("http://localhost:4200?birdName=%s", url.QueryEscape(birdName)))
+    res, err := http.Get(fmt.Sprintf("http://birdimageapi:4200/?birdName=%s", url.QueryEscape(birdName)))
     if err != nil {
         return "", err
     }
